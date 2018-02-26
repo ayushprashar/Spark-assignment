040775bda1175211691a39f0a4ad3a8971f12eed
Ayush Prashar <ayush.prashar@knoldus.in>
2018-02-26 10:59:39 +0530

    Final Draft

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,3 +1,12 @@
+import Operations.Functionalities
+import org.apache.spark.rdd.RDD
 class MainApp {
+  val sales: String = "./files/sales"
+  val customer: String = "./files/customer"
+  val splitToken: String = "#"
+  def main (args: Array[String]): Unit = {
+    val operate: Functionalities = Functionalities(customer,sales,splitToken)
+    val resultRDD: RDD[(Long, (String, String))] = operate.getCustomerSales
 
+  }
 }
