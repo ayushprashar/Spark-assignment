884eadf69c89dcdeb6fe6d177824748975c1cb0f
Ayush Prashar <ayush.prashar@knoldus.in>
2018-02-26 11:03:56 +0530

    With main

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,12 +1,12 @@
 import Operations.Functionalities
-import org.apache.spark.rdd.RDD
+
 class MainApp {
   val sales: String = "./files/sales"
   val customer: String = "./files/customer"
   val splitToken: String = "#"
   def main (args: Array[String]): Unit = {
     val operate: Functionalities = Functionalities(customer,sales,splitToken)
-    val resultRDD: RDD[(Long, (String, String))] = operate.getCustomerSales
-
+    operate.result(operate.getCustomerSales)
+    operate.sparkContext.stop()
   }
 }
